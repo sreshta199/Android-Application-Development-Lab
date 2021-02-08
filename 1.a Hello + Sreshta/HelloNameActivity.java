eb593c794bcc8ad825c10c1977e28f90b0cc44a1
sreshta199 <78163009+sreshta199@users.noreply.github.com>
2021-02-08 14:28:33 +0530

    Create HelloNameActivity.java

---
+++ 1.a Hello + Sreshta/HelloNameActivity.java
@@ -0,0 +1,14 @@
+package com.example.android_class;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HelloNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_name);
+    }
+}
